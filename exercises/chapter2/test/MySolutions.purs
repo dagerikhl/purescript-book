d4630a3e5cc0f174b91fd5b7984d3d01f5511e50
Løvgren, Dag Erik <dagerikhl@gmail.com>
2021-11-21 15:00:13 +0100

    Solve chapter 2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,17 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Math (pi, pow, sqrt)
+import Data.Int (rem)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+-- Exercise 1
+circleArea :: Number -> Number
+circleArea r = pi * pow r 2.0
+
+-- Exercise 2
+leftoverCents :: Int -> Int
+leftoverCents v = rem v 100
